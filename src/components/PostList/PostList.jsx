77e54d9fc4d3cd954b28450917e232ee9f3655c6
Tetiana Pyatachuk <pyatachuk26@gmail.com>
2026-02-14 09:44:37 +0200

    my Statistik Posts

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,16 @@
-export const PostList = () => <>Put the list here</>;
+// add styles here
+import { PostInfo } from '../PostInfo/PostInfo';
+
+export const PostList = ({ posts }) => {
+  if (!posts.length) {
+    return null;
+  }
+
+  return (
+    <>
+      {posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
